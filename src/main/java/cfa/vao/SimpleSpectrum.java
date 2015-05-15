2b578a8ddc22aa4a0b07898ba8bdf20c0f6f1262
Omar Laurino <olaurino@cfa.harvard.edu>
2015-05-14 23:06:30 -0400

    add errorbars

--- src/main/java/cfa/vao/SimpleSpectrum.java
+++ src/main/java/cfa/vao/SimpleSpectrum.java
@@ -33,12 +33,27 @@ public class SimpleSpectrum implements BeanView<SpectralDataset> {
         return y;
     }
 
-    public Double[] getYErr(SpectralDataset ds) {
+    public Double[] getYErrLow(SpectralDataset ds) {
         int len = ds.getData().size();
         Double[] err = new Double[len];
         for (int i=0; i<len; i++) {
             try {
-                err[i] = ds.getData(i).getFluxAxis().getAccuracy().getStatError().getValue();
+                Double flux = ds.getData(i).getFluxAxis().getValue().getValue();
+                err[i] = flux - ds.getData(i).getFluxAxis().getAccuracy().getStatError().getValue();
+            } catch(Exception ex) {
+                err[i] = Double.NaN;
+            }
+        }
+        return err;
+    }
+
+    public Double[] getYErrHigh(SpectralDataset ds) {
+        int len = ds.getData().size();
+        Double[] err = new Double[len];
+        for (int i=0; i<len; i++) {
+            try {
+                Double flux = ds.getData(i).getFluxAxis().getValue().getValue();
+                err[i] = flux + ds.getData(i).getFluxAxis().getAccuracy().getStatError().getValue();
             } catch(Exception ex) {
                 err[i] = Double.NaN;
             }
