e1fce256cb0b84bc1c8eabc57f47d54cd8ab4366
Omar Laurino <olaurino@cfa.harvard.edu>
2015-06-09 16:27:24 -0400

    handle exceptions, remove genson isSet* hack

--- src/main/java/cfa/vao/App.java
+++ src/main/java/cfa/vao/App.java
@@ -20,10 +20,7 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.URL;
 import java.util.HashMap;
@@ -37,42 +34,56 @@ import java.util.logging.Logger;
  */
 public class App 
 {
-    public static void main( String[] args ) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        HttpContext context = server.createContext("/test", new Handler());
-        context.getFilters().add(new ParameterFilter());
-        server.setExecutor(null);
-        server.start();
+    public static void main( String[] args ) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+            HttpContext context = server.createContext("/test", new Handler());
+            context.getFilters().add(new ParameterFilter());
+            server.setExecutor(null);
+            server.start();
+        } catch (Throwable ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     private static class Handler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
-            Map<String, Object> params = (Map<String, Object>) t.getAttribute("parameters");
-            String url = (String) params.get("ds");
-            String prefix = (String) params.get("prefix");
-            SpectralDataset ds = getDatasetForURL(url, prefix);
-            Genson genson = make_genson();
-            String meta = genson.serialize(ds);
-            String data = genson.serialize(ds, SimpleSpectrum.class);
-
-            Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
-            cfg.setClassForTemplateLoading(App.class, "/");
-            Template template = cfg.getTemplate("page.html");
-            Map dict = new HashMap<String, Object>();
-            dict.put("metadata", meta);
-            dict.put("data", data);
-
-            t.sendResponseHeaders(200, 0);
-            OutputStream os = t.getResponseBody();
-            Writer w = new OutputStreamWriter(os);
             try {
-                template.process(dict, w);
+                Map<String, Object> params = (Map<String, Object>) t.getAttribute("parameters");
+                String url = (String) params.get("ds");
+                String prefix = (String) params.get("prefix");
+                SpectralDataset ds = getDatasetForURL(url, prefix);
+                Genson genson = make_genson();
+                String meta = genson.serialize(ds);
+                String data = genson.serialize(ds, SimpleSpectrum.class);
+
+                Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
+                cfg.setClassForTemplateLoading(App.class, "/");
+                Template template = cfg.getTemplate("page.html");
+                Map dict = new HashMap<String, Object>();
+                dict.put("metadata", meta);
+                dict.put("data", data);
+
+                t.sendResponseHeaders(200, 0);
+                OutputStream os = t.getResponseBody();
+                Writer w = new OutputStreamWriter(os);
+                try {
+                    template.process(dict, w);
+                    w.close();
+                } catch (TemplateException e) {
+                    Logger.getLogger(Handler.class.getName()).log(Level.SEVERE, e.getMessage(), e);
+                }
+                //            os.write(response.getBytes());
+                os.close();
+            } catch (Throwable ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                t.sendResponseHeaders(500, 0);
+                OutputStream os = t.getResponseBody();
+                Writer w = new OutputStreamWriter(os);
+                ex.printStackTrace(new PrintWriter(w));
                 w.close();
-            } catch (TemplateException e) {
-                Logger.getLogger(Handler.class.getName()).log(Level.SEVERE, e.getMessage(), e);
+                os.close();
             }
-//            os.write(response.getBytes());
-            os.close();
         }
     }
 
@@ -82,8 +93,6 @@ public class App
         }
 
         URL file = new URL(url);
-//        URL file = new URL("http://dc.g-vo.org/flashheros/q/sdl/dlget?ID=ivo%3A//org.gavo.dc/%7E%3Fflashheros/data/ca92/f0006.mt&FORMAT=application/x-votable%2Bxml%3Bcontent%3Dspec2");
-//        URL file = new URL("file:///Users/olaurino/Downloads/result.vot");
 
         VOTableIO instance = new VOTableIO();
         Model model = new DefaultModelBuilder("Spectrum-2.0")
@@ -125,65 +134,6 @@ public class App
                 .rename("sNR", "SNR")
                 .rename("uRL", "URL")
                 .rename("uCD", "UCD")
-                .exclude("setID")
-                .exclude("setIdentifier")
-                .exclude("setModelPath")
-                .exclude("setName")
-                .exclude("setDescription")
-                .exclude("setUCD")
-                .exclude("setUnit")
-                .exclude("setUtype")
-                .exclude("setValue")
-                .exclude("setDataProductType")
-                .exclude("setDataProductSubtype")
-                .exclude("setCalibLevel")
-                .exclude("setDataModel")
-                .exclude("setCuration")
-                .exclude("setCharacterization")
-                .exclude("setCoordSys")
-                .exclude("setDataID")
-                .exclude("setDerived")
-                .exclude("setObsConfig")
-                .exclude("setProposal")
-                .exclude("setTarget")
-                .exclude("setData")
-                .exclude("setFluxSI")
-                .exclude("setSpactralSI")
-                .exclude("setTimeSI")
-                .exclude("setRedshift")
-                .exclude("setSNR")
-                .exclude("setVarAmpl")
-                .exclude("setConfidence")
-                .exclude("setStatError")
-                .exclude("setPrefix")
-                .exclude("setURL")
-                .exclude("setCollections")
-                .exclude("setContributors")
-                .exclude("setCreationType")
-                .exclude("setCreator")
-                .exclude("setCreatorDID")
-                .exclude("setDatasetID")
-                .exclude("setDate")
-                .exclude("setLogo")
-                .exclude("setObservationID")
-                .exclude("setVersion")
-                .exclude("setTitle")
-                .exclude("setContact")
-                .exclude("setEmail")
-                .exclude("setPublisher")
-                .exclude("setPublisherDID")
-                .exclude("setPublisherID")
-                .exclude("setReferences")
-                .exclude("setReleaseDate")
-                .exclude("setRights")
-                .exclude("setCharacterizationAxes")
-                .exclude("setEquinox")
-                .exclude("setReferencePosition")
-                .exclude("setZero")
-                .exclude("setDopplerDefinition")
-                .exclude("setSpectralSI")
-                .exclude("setObservingElements")
-                .exclude("setIdentified")
                 .exclude("modelpath")
                 .exclude("length")
                 .useIndentation(true)
